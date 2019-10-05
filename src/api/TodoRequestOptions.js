bea21a0b0f99aa185943b02e55b7bb59dfadfc41
yuvalschneider <yuval@yuvalschneider.com>
2019-10-05 13:10:13 +0300

    fix port to 3000

--- src/api/TodoRequestOptions.js
+++ src/api/TodoRequestOptions.js
@@ -4,7 +4,7 @@ class TodoRequestOptions {
 	constructor(host, todoUrl) {
 		this.host = host ? host : 'http://' + 
  			(process.env.TODO_BACKEND ? process.env.TODO_BACKEND : 'localhost') + ':' + 
- 			(process.env.TODO_BACKEND_PORT ? process.env.TODO_BACKEND_PORT : '3001') + '/';
+ 			(process.env.TODO_BACKEND_PORT ? process.env.TODO_BACKEND_PORT : '3000') + '/';
 		this.todoUrl = todoUrl ? todoUrl : 'api/Todos/';
 	}
 
